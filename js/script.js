160f0e1169b664452be69640fc0c8ebca6b2deb7
Stefano <stefano.zaccaria29@gmail.com>
2020-06-15 17:31:01 +0200

    ex js-trainTicket update!

--- js/script.js
+++ js/script.js
@@ -5,17 +5,14 @@ var oldSales;
 var age = parseInt(prompt('quanti anni hai?'));
 var km = parseInt(prompt('quanti kilometri vuoi percorrere?'));
 
-console.log(age);
-console.log(km);
+document.getElementById('age').innerHTML = age;
+document.getElementById('km').innerHTML = km;
 
 var price = (km * 0.21).toFixed(2);
 
 var youngSales = (price * 20 / 100).toFixed(2);
-console.log(youngSales);
 
 var oldSales = (price * 40 / 100).toFixed(2);
-console.log(oldSales);
-
 
 if ( age < 18 ) {
   price = (price - youngSales).toFixed(2);
@@ -25,4 +22,4 @@ if ( age >= 65 ) {
   price = (price - oldSales).toFixed(2);
 }
 
-document.getElementById('price').innerHTML = "questo e' il prezzo per te " + price;
+document.getElementById('price').innerHTML = price;
